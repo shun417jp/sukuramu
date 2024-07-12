90fc2be3a4a1fc4e158af114d48aa231554d6579
Tomaki.s <s.tomaki.sys23@morijyobi.ac.jp>
2024-07-12 09:28:41 +0900

    .gitignoreを追加した

---
+++ Player.java
@@ -0,0 +1,25 @@
+public class Player {
+
+    private String playerName;
+ 
+    public Player(String playerName) {
+ 
+        this.playerName = playerName;
+ 
+    }
+ 
+    public String getPlayerName() {
+ 
+        return playerName;
+ 
+    }
+ 
+    public void setPlayerName(String playerName) {
+ 
+        this.playerName = playerName;
+ 
+    }
+ 
+ }
+ 
+ 

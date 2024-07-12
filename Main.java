ed5ff7f1caa5a15b1edc8bbf4a44f8d53b05587d
Tomaki.s <s.tomaki.sys23@morijyobi.ac.jp>
2024-07-12 10:46:28 +0900

    CPUの手をランダムにする処理を実装

--- Main.java
+++ Main.java
@@ -92,7 +92,15 @@ public class Main {
 
        //以降にテストコードを追加して検証を行う
 
+ //comの手を生成
 
+ com.createRandomHand();
+
+ //以降にテストコードを追加して検証を行う
+
+ System.out.println("ランダムなジャンケンの手をCPUにセットする");
+
+System.out.println(com.getHand().getHandName());
 
    }
 
